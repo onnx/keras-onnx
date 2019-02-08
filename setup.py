88835b54de6088a23c7a234d11087a65dc4f5f1e
Wenbing Li <wenli@microsoft.com>
2019-02-07 17:15:08 -0800

    Minor fixing for the publish.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with (open(os.path.join(this, '{}/__init__.py'.format(pkgname)), "r")) as f:
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
-    start_pos = long_description.find('## Introduction')
+    start_pos = long_description.find('# Introduction')
     if start_pos >= 0:
         long_description = long_description[start_pos:]
 
@@ -46,19 +46,19 @@ setup(
     license='MIT License',
     author='Microsoft Corporation',
     author_email='winmlcvt@microsoft.com',
-    url='https://microsoft.com',
+    url='https://github.com/onnx/keras-onnx',
     packages=packages,
     include_package_data=True,
     install_requires=requirements,
     tests_require=['pytest', 'pytest-cov'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'Operating System :: Linux',
+        'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'License :: OSI Approved :: MIT License'],
+        'License :: OSI Approved :: MIT License']
 )
