d7942a7764b8d54803aecdd9cfb9edb13ee94db2
David Fan <30608893+jiafatom@users.noreply.github.com>
2019-03-07 17:55:20 -0800

    Set default axis=-1 for softmax for keras (#37)
    * Set default softmax axis=-1
    
    * Set default softmax axis=-1

--- keras2onnx/ke2onnx/dense.py
+++ keras2onnx/ke2onnx/dense.py
@@ -5,20 +5,12 @@
 # --------------------------------------------------------------------------
 
 import keras
-from keras.activations import get as _get_activation
+from keras.activations import get
 from ..proto import onnx_proto
 from ..common.onnx_ops import apply_sigmoid, apply_softmax, apply_identity, apply_relu, apply_add
 from ..common.onnx_ops import apply_elu, apply_selu, apply_tanh, apply_hard_sigmoid
 import numpy as np
-
-_activation_map = {_get_activation('sigmoid'): apply_sigmoid,
-                   _get_activation('softmax'): apply_softmax,
-                   _get_activation('linear'): apply_identity,
-                   _get_activation('relu'): apply_relu,
-                   _get_activation('elu'): apply_elu,
-                   _get_activation('selu'): apply_selu,
-                   _get_activation('tanh'): apply_tanh,
-                   _get_activation('hard_sigmoid'): apply_hard_sigmoid}
+from .activation import activation_map
 
 
 def convert_keras_dense(scope, operator, container):
@@ -46,8 +38,8 @@ def convert_keras_dense(scope, operator, container):
               axis=-1, broadcast=1)
 
     # Create an activation function node and apply activation function to the intermediate tensor
-    apply_activation_function = _activation_map[operator.raw_operator.activation]
-    if apply_activation_function in [_get_activation('softmax'), keras.activations.softmax]:
+    apply_activation_function = activation_map[operator.raw_operator.activation]
+    if apply_activation_function in [get('softmax'), keras.activations.softmax]:
         apply_softmax(scope, biased_tensor_name, operator.outputs[0].full_name, container, axis=-1)
     else:
         apply_activation_function(scope, biased_tensor_name, operator.outputs[0].full_name, container)
