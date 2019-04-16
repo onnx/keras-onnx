10494e0113898f0f8154039e3416cb84cc6db9fa
Wenbing Li <wenli@microsoft.com>
2019-04-15 17:07:49 -0700

    update the main.py

--- keras2onnx/__init__.py
+++ keras2onnx/__init__.py
@@ -15,6 +15,16 @@ __producer_version__ = __version__
 __domain__ = "onnx"
 __model_version__ = 0
 
+try:
+    import sys
+    import os.path
+    from os.path import dirname, abspath
+    import tensorflow
+    sys.path.insert(0, os.path.join(dirname(abspath(__file__)), 'ktf2onnx'))
+except ImportError:
+    raise AssertionError('Please conda install / pip install tensorflow or tensorflow-gpu before the model conversion.')
+
+
 
 from .common import Variable, cvtfunc, set_logger_level
 from .funcbook import set_converter
@@ -25,11 +35,3 @@ from .main import convert_keras
 def tfname_to_onnx(name): return Variable.tfname_to_onnx(name)
 
 
-try:
-    import sys
-    import os.path
-    from os.path import dirname, abspath
-    import tensorflow
-    sys.path.insert(0, os.path.join(dirname(abspath(__file__)), 'ktf2onnx'))
-except ImportError:
-    raise AssertionError('Please conda install / pip install tensorflow or tensorflow-gpu before the model conversion.')
