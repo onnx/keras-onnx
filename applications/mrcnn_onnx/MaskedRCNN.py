e2c8bc615e96dfc91f0eae0e352e239f80612c92
Wenbing Li <wenli@microsoft.com>
2019-05-24 11:49:04 -0700

    fix the detection layer issue.

--- applications/mrcnn_onnx/MaskedRCNN.py
+++ applications/mrcnn_onnx/MaskedRCNN.py
@@ -1,15 +1,11 @@
 import os
 import sys
-import time
 import numpy as np
 import skimage
 import onnx
 import keras2onnx
 
-from pycocotools.coco import COCO
-from pycocotools.cocoeval import COCOeval
-from pycocotools import mask as maskUtils
-
+from timeit import default_timer as timer
 from mrcnn.config import Config
 from mrcnn import model as modellib, utils
 
@@ -47,231 +43,6 @@ class CocoConfig(Config):
 
     # Number of classes (including background)
     NUM_CLASSES = 1 + 80  # COCO has 80 classes
-
-
-class CocoDataset(utils.Dataset):
-    def load_coco(self, dataset_dir, subset, year=DEFAULT_DATASET_YEAR, class_ids=None,
-                  class_map=None, return_coco=False):
-        """Load a subset of the COCO dataset.
-        dataset_dir: The root directory of the COCO dataset.
-        subset: What to load (train, val, minival, valminusminival)
-        year: What dataset year to load (2014, 2017) as a string, not an integer
-        class_ids: If provided, only loads images that have the given classes.
-        class_map: TODO: Not implemented yet. Supports maping classes from
-            different datasets to the same class ID.
-        return_coco: If True, returns the COCO object.
-        auto_download: Automatically download and unzip MS-COCO images and annotations
-        """
-
-        coco = COCO("{}/annotations/instances_{}{}.json".format(dataset_dir, subset, year))
-        if subset == "minival" or subset == "valminusminival":
-            subset = "val"
-        image_dir = "{}/{}{}".format(dataset_dir, subset, year)
-
-        # Load all classes or a subset?
-        if not class_ids:
-            # All classes
-            class_ids = sorted(coco.getCatIds())
-
-        # All images or a subset?
-        if class_ids:
-            image_ids = []
-            for id in class_ids:
-                image_ids.extend(list(coco.getImgIds(catIds=[id])))
-            # Remove duplicates
-            image_ids = list(set(image_ids))
-        else:
-            # All images
-            image_ids = list(coco.imgs.keys())
-
-        # Add classes
-        for i in class_ids:
-            self.add_class("coco", i, coco.loadCats(i)[0]["name"])
-
-        # Add images
-        for i in image_ids:
-            self.add_image(
-                "coco", image_id=i,
-                path=os.path.join(image_dir, coco.imgs[i]['file_name']),
-                width=coco.imgs[i]["width"],
-                height=coco.imgs[i]["height"],
-                annotations=coco.loadAnns(coco.getAnnIds(
-                    imgIds=[i], catIds=class_ids, iscrowd=None)))
-        if return_coco:
-            return coco
-
-    def load_mask(self, image_id):
-        """Load instance masks for the given image.
-
-        Different datasets use different ways to store masks. This
-        function converts the different mask format to one format
-        in the form of a bitmap [height, width, instances].
-
-        Returns:
-        masks: A bool array of shape [height, width, instance count] with
-            one mask per instance.
-        class_ids: a 1D array of class IDs of the instance masks.
-        """
-        # If not a COCO image, delegate to parent class.
-        image_info = self.image_info[image_id]
-        if image_info["source"] != "coco":
-            return super(CocoDataset, self).load_mask(image_id)
-
-        instance_masks = []
-        class_ids = []
-        annotations = self.image_info[image_id]["annotations"]
-        # Build mask of shape [height, width, instance_count] and list
-        # of class IDs that correspond to each channel of the mask.
-        for annotation in annotations:
-            class_id = self.map_source_class_id(
-                "coco.{}".format(annotation['category_id']))
-            if class_id:
-                m = self.annToMask(annotation, image_info["height"],
-                                   image_info["width"])
-                # Some objects are so small that they're less than 1 pixel area
-                # and end up rounded out. Skip those objects.
-                if m.max() < 1:
-                    continue
-                # Is it a crowd? If so, use a negative class ID.
-                if annotation['iscrowd']:
-                    # Use negative class ID for crowds
-                    class_id *= -1
-                    # For crowd masks, annToMask() sometimes returns a mask
-                    # smaller than the given dimensions. If so, resize it.
-                    if m.shape[0] != image_info["height"] or m.shape[1] != image_info["width"]:
-                        m = np.ones([image_info["height"], image_info["width"]], dtype=bool)
-                instance_masks.append(m)
-                class_ids.append(class_id)
-
-        # Pack instance masks into an array
-        if class_ids:
-            mask = np.stack(instance_masks, axis=2).astype(np.bool)
-            class_ids = np.array(class_ids, dtype=np.int32)
-            return mask, class_ids
-        else:
-            # Call super class to return an empty mask
-            return super(CocoDataset, self).load_mask(image_id)
-
-    def image_reference(self, image_id):
-        """Return a link to the image in the COCO Website."""
-        info = self.image_info[image_id]
-        if info["source"] == "coco":
-            return "http://cocodataset.org/#explore?id={}".format(info["id"])
-        else:
-            super(CocoDataset, self).image_reference(image_id)
-
-    # The following two functions are from pycocotools with a few changes.
-
-    def annToRLE(self, ann, height, width):
-        """
-        Convert annotation which can be polygons, uncompressed RLE to RLE.
-        :return: binary mask (numpy 2D array)
-        """
-        segm = ann['segmentation']
-        if isinstance(segm, list):
-            # polygon -- a single object might consist of multiple parts
-            # we merge all parts into one mask rle code
-            rles = maskUtils.frPyObjects(segm, height, width)
-            rle = maskUtils.merge(rles)
-        elif isinstance(segm['counts'], list):
-            # uncompressed RLE
-            rle = maskUtils.frPyObjects(segm, height, width)
-        else:
-            # rle
-            rle = ann['segmentation']
-        return rle
-
-    def annToMask(self, ann, height, width):
-        """
-        Convert annotation which can be polygons, uncompressed RLE, or RLE to binary mask.
-        :return: binary mask (numpy 2D array)
-        """
-        rle = self.annToRLE(ann, height, width)
-        m = maskUtils.decode(rle)
-        return m
-
-
-############################################################
-#  COCO Evaluation
-############################################################
-
-def build_coco_results(dataset, image_ids, rois, class_ids, scores, masks):
-    """Arrange resutls to match COCO specs in http://cocodataset.org/#format
-    """
-    # If no results, return an empty list
-    if rois is None:
-        return []
-
-    results = []
-    for image_id in image_ids:
-        # Loop through detections
-        for i in range(rois.shape[0]):
-            class_id = class_ids[i]
-            score = scores[i]
-            bbox = np.around(rois[i], 1)
-            mask = masks[:, :, i]
-
-            result = {
-                "image_id": image_id,
-                "category_id": dataset.get_source_class_id(class_id, "coco"),
-                "bbox": [bbox[1], bbox[0], bbox[3] - bbox[1], bbox[2] - bbox[0]],
-                "score": score,
-                "segmentation": maskUtils.encode(np.asfortranarray(mask))
-            }
-            results.append(result)
-    return results
-
-
-def evaluate_coco(model, dataset, coco, eval_type="bbox", limit=0, image_ids=None):
-    """Runs official COCO evaluation.
-    dataset: A Dataset object with valiadtion data
-    eval_type: "bbox" or "segm" for bounding box or segmentation evaluation
-    limit: if not 0, it's the number of images to use for evaluation
-    """
-    # Pick COCO images from the dataset
-    image_ids = image_ids or dataset.image_ids
-
-    # Limit to a subset
-    if limit:
-        image_ids = image_ids[:limit]
-
-    # Get corresponding COCO image IDs.
-    coco_image_ids = [dataset.image_info[id]["id"] for id in image_ids]
-
-    t_prediction = 0
-    t_start = time.time()
-
-    results = []
-    for i, image_id in enumerate(image_ids):
-        # Load image
-        image = dataset.load_image(image_id)
-
-        # Run detection
-        t = time.time()
-        r = model.detect([image], verbose=0)[0]
-        t_prediction += (time.time() - t)
-
-        # Convert results to COCO format
-        # Cast masks to uint8 because COCO tools errors out on bool
-        image_results = build_coco_results(dataset, coco_image_ids[i:i + 1],
-                                           r["rois"], r["class_ids"],
-                                           r["scores"],
-                                           r["masks"].astype(np.uint8))
-        results.extend(image_results)
-
-    # Load results. This modifies results with additional attributes.
-    coco_results = coco.loadRes(results)
-
-    # Evaluate
-    cocoEval = COCOeval(coco, coco_results, eval_type)
-    cocoEval.params.imgIds = coco_image_ids
-    cocoEval.evaluate()
-    cocoEval.accumulate()
-    cocoEval.summarize()
-
-    print("Prediction time: {}. Average {}/image".format(
-        t_prediction, t_prediction / len(image_ids)))
-    print("Total time: ", time.time() - t_start)
 
 
 class InferenceConfig(CocoConfig):
@@ -363,9 +134,11 @@ def convert_BatchNorm(scope, operator, container):
     convert_keras_batch_normalization(scope, operator, container)
 
 
-
+from keras2onnx.common.onnx_ops import OnnxOperatorBuilder
 
 def convert_DetectionLayer(scope, operator, container):
+
+    oopb = OnnxOperatorBuilder(container, scope)
     # type: (keras2onnx.common.InterimContext, keras2onnx.common.Operator, keras2onnx.common.OnnxObjectContainer) -> None
     DETECTION_MAX_INSTANCES = 100
     DETECTION_NMS_THRESHOLD = 0.3
@@ -380,7 +153,14 @@ def convert_DetectionLayer(scope, operator, container):
     score_identity = scope.get_unique_variable_name(operator.inputs[1].full_name + '_id')
     apply_identity(scope, operator.inputs[1].full_name, score_identity, container)
     # output shape: [num_batches, spatial_dimension, num_classes]
-    apply_transpose(scope, score_identity, score_transpose, container, perm=[0, 2, 1])
+    sliced_score = oopb.add_node('Slice',
+                                 [score_identity,
+                                  ('_start', oopb.int64, np.array([1], dtype='int64')),
+                                  ('_end', oopb.int64, np.array([81], dtype='int64')),
+                                  ('_axes', oopb.int64, np.array([2], dtype='int64'))
+                                  ],
+                                 operator.inputs[1].full_name + '_sliced')
+    apply_transpose(scope, sliced_score, score_transpose, container, perm=[0, 2, 1])
     # output shape: [num_batches, num_classes, spatial_dimension]
 
 
@@ -402,6 +182,7 @@ def convert_DetectionLayer(scope, operator, container):
                               [], [DETECTION_NMS_THRESHOLD])
     container.add_initializer(score_threshold, onnx_proto.TensorProto.FLOAT,
                               [], [DETECTION_MIN_CONFIDENCE])
+
 
     nms_node = next((nd_ for nd_ in operator.node_list if nd_.type == 'NonMaxSuppressionV3'), operator.node_list[0])
     nms_output = scope.get_unique_variable_name(operator.output_full_names[0] + '_nms')
@@ -539,6 +320,20 @@ def convert_DetectionLayer(scope, operator, container):
     container.add_node('TopK', [score_gather, top_k_var], [score_top_k_output_val, score_top_k_output_idx], op_version=operator.target_opset,
                        name=nms_node.name + '_topK', **attrs)
 
+    # nonzero_classid = oopb.add_node('NonZero',
+    #                                 [class_idx_output],
+    #                                 nms_node.name + '_nonzero_classid',
+    #                                 )
+    # nonzero_reshaped = oopb.add_node('Reshape',
+    #                                  [nonzero_classid,
+    #                                   ('shape', oopb.int64, np.array([-1]))],
+    #                                  nms_node.name + '_nonzero_reshaped')
+    #
+    # intersection_idx = oopb.add_node('DenseToDenseSetOperation',
+    #                                            [nonzero_reshaped, score_top_k_output_idx],
+    #                                            nms_node.name + '_intersection',
+    #                                            op_domain='com.microsoft')
+
     '''
     box_unsqueeze = scope.get_unique_variable_name(operator.output_full_names[0] + '_box_unsqueeze')
     attrs = {'axes': [1]}
@@ -586,6 +381,20 @@ def convert_DetectionLayer(scope, operator, container):
                        op_version=operator.target_opset,
                        name=nms_node.name + '_all_gather', **attrs)
     # output shape: [num_top_K, 6]
+    padded_result = oopb.add_node('DynamicPad',
+                                  [all_gather,
+                                   np.array([0, 0, DETECTION_MAX_INSTANCES, 0],
+                                            dtype=np.int64)],
+                                  nms_node.name + '_padded_result',
+                                  op_domain='com.microsoft')
+    detection_final = oopb.add_node('Slice',
+                                 [padded_result,
+                                  ('_start', oopb.int64, np.array([0], dtype='int64')),
+                                  ('_end', oopb.int64, np.array([DETECTION_MAX_INSTANCES], dtype='int64')),
+                                  ('_axes', oopb.int64, np.array([0], dtype='int64'))
+                                  ],
+                                 nms_node.name + '_detection_final'
+                                 )
 
     '''
     starts_init_4 = scope.get_unique_variable_name('starts')
@@ -666,7 +475,7 @@ def convert_DetectionLayer(scope, operator, container):
     attrs = {'axes': [0]}
     container.add_node("Unsqueeze",
                        # pad_concat,
-                       all_gather,
+                       detection_final,
                        operator.output_full_names[0],
                        op_version=operator.target_opset,
                        name=nms_node.name + '_concat_unsqueeze', **attrs)
@@ -721,13 +530,15 @@ if len(sys.argv) > 1 and sys.argv[1] == '-c':
 else:
     # run with ONNXRuntime
     import onnxruntime
-    input("the python process id is %d, press ENTER key to cont..." % os.getpid())
+    print("The python process id is %d, attach the VS as the debugger." % os.getpid())
 
     molded_images, image_metas, windows = model.mold_inputs([image])
     anchors = model.get_anchors(molded_images[0].shape)
     anchors = np.broadcast_to(anchors, (model.config.BATCH_SIZE,) + anchors.shape)
 
+    start = timer()
     results_k = model.keras_model.predict([molded_images.astype(np.float32), image_metas.astype(np.float32), anchors])
+    print("Total Keras inf time is %fs " % (timer() - start))
 
     sess = onnxruntime.InferenceSession('./mrcnn.onnx')
     '''
@@ -736,10 +547,14 @@ else:
                         "input_anchors:01": anchors,
                         "input_image_meta:01": image_metas.astype(np.float32)})
     '''
+    start_ort = timer()
     results =\
         sess.run(None, {"input_image:01": molded_images.astype(np.float32),
                         "input_anchors:01": anchors,
                         "input_image_meta:01": image_metas.astype(np.float32)})
+    print("Total ORT inf time is %fs " % (timer() - start_ort))
+
+
     rtol = 1.e-2 #1.e-3
     atol = 1.e-4 #1.e-6
     for n_ in range(len(results)):
--- keras2onnx/common/onnx_ops.py
+++ keras2onnx/common/onnx_ops.py
@@ -3,5 +3,57 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 ###############################################################################
+from .interim import OnnxObjectContainer, InterimContext
+from ..proto import onnx_proto
+from onnx.mapping import NP_TYPE_TO_TENSOR_TYPE
 
-from onnxconverter_common.onnx_ops import *
+from onnxconverter_common.onnx_ops import *  # noqa:
+
+
+class OnnxOperatorBuilder:
+    def __init__(self, container, scope):
+        # type: (OnnxOperatorBuilder, OnnxObjectContainer, InterimContext) -> None
+        self._container = container
+        self._scope = scope
+        self.int32 = onnx_proto.TensorProto.INT32
+        self.int64 = onnx_proto.TensorProto.INT64
+        self.float = onnx_proto.TensorProto.FLOAT
+        self.double = onnx_proto.TensorProto.DOUBLE
+
+    def _process_inputs(self, inputs, name):
+        ox_inputs = []
+        for i_ in inputs:
+            ox_n = self._scope.get_unique_variable_name(name + '_i')
+            if isinstance(i_, np.ndarray):
+                self._container.add_initializer(
+                    ox_n,
+                    NP_TYPE_TO_TENSOR_TYPE[i_.dtype],
+                    i_.shape,
+                    i_.flatten()
+                )
+            elif isinstance(i_, (tuple, list)):
+                ox_n = self._scope.get_unique_variable_name(name + i_[0])
+                self._container.add_initializer(
+                    ox_n,
+                    i_[1],
+                    i_[2].shape,
+                    i_[2].flatten()
+                )
+            elif isinstance(i_, str):
+                ox_n = i_
+            else:
+                raise RuntimeError('Unknown type for ONNX initializer: {}'.format(type(i_)))
+            ox_inputs.append(ox_n)
+
+        return ox_inputs
+
+    def add_node_all(self, op_type, inputs, name, outputs_num=1, op_domain='', op_version=None, **attrs):
+        if op_version is None:
+            op_version = self._container.target_opset
+        outputs = [self._scope.get_unique_variable_name(name + str(i_)) for i_ in range(outputs_num)]
+        ox_inputs = self._process_inputs(inputs, name)
+        self._container.add_node(op_type, ox_inputs, outputs, op_domain, op_version, name=name, **attrs)
+        return outputs
+
+    def add_node(self, op_type, inputs, name, op_domain='', op_version=None, **attrs):
+        return self.add_node_all(op_type, inputs, name, 1, op_domain, op_version, **attrs)[0]
