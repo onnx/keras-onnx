994c31d46a623dee17b737c13cf5580df3d11eb7
Vinitra Swamy <vinitras@gmail.com>
2019-05-29 13:29:17 -0700

    Adding unit tests for average pooling

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -339,6 +339,20 @@ class TestKerasTF2ONNX(unittest.TestCase):
         expected = model.predict(data)
         self.assertTrue(self.run_onnx_runtime(onnx_model.graph.name, onnx_model, data, expected))
 
+    def test_pooling_avg1d(self):
+        self._pooling_test_helper(keras.layers.AveragePooling1D, (4, 4))
+
+    def test_pooling_avg2d(self):
+        N, C, H, W = 2, 3, 5, 5
+        x = np.random.rand(N, H, W, C).astype(np.float32, copy=False)
+
+        model = keras.models.Sequential()
+        model.add(keras.layers.AveragePooling2D((2, 2), strides=(2, 2), input_shape=(H, W, C), data_format='channels_last'))
+        model.compile(optimizer='sgd', loss='mse')
+        onnx_model = keras2onnx.convert_keras(model, model.name, target_opset=10)
+        expected = model.predict(x)
+        self.assertTrue(self.run_onnx_runtime(onnx_model.graph.name, onnx_model, x, expected))
+
     @unittest.skip("ONNXRuntime doesn't support 3D average pooling yet.")
     def test_pooling_avg3d(self):
         self._pooling_test_helper(keras.layers.AveragePooling3D, (4, 4, 4, 3))
@@ -353,7 +367,7 @@ class TestKerasTF2ONNX(unittest.TestCase):
         model = keras.models.Sequential()
         model.add(keras.layers.MaxPooling2D((2, 2), strides=(2, 2), input_shape=(H, W, C), data_format='channels_last'))
         model.compile(optimizer='sgd', loss='mse')
-        onnx_model = keras2onnx.convert_keras(model, model.name) 
+        onnx_model = keras2onnx.convert_keras(model, model.name)
         expected = model.predict(x)
         self.assertTrue(self.run_onnx_runtime('max_pooling_2d', onnx_model, x, expected))
 
