8753c74403c6d7c018a56fe6ed557d1097f59c64
David Fan <jiafa@microsoft.com>
2019-06-03 15:10:52 -0700

    Add unit tests for upsample and pool

--- keras2onnx/ke2onnx/pooling.py
+++ keras2onnx/ke2onnx/pooling.py
@@ -9,9 +9,9 @@ from .common import get_permutation_config
 def convert_keras_pooling_core(scope, operator, container, is_global, n_dims,
                                op_type, input_perm_axes, output_perm_axes):
     op = operator.raw_operator
-    channels_first = n_dims > 1 and op.data_format == 'channels_first'
+    no_permutation_required = op.data_format == 'channels_first'
 
-    if channels_first:
+    if no_permutation_required:
         adjusted_pooling_input = operator.inputs[0].full_name
     else:
         adjusted_pooling_input = scope.get_unique_variable_name('input_transposed')
@@ -23,9 +23,14 @@ def convert_keras_pooling_core(scope, operator, container, is_global, n_dims,
     op_type_prefix = 'Global' if is_global else ''
     onnx_op_type = "AveragePool" if op_type == 'Avg' else 'MaxPool'
     attrs = {'name': operator.full_name}
+    op_version = 10 if container.target_opset >= 10 else 7
     if not is_global:
         attrs['strides'] = list(op.strides)
         attrs['kernel_shape'] = op.pool_size
+        attrs['op_version'] = op_version
+        # In ONNX opset 10, the ceil_mode attribute was added to local MaxPool and AveragePool
+        if container.target_opset >= 10:
+            attrs['ceil_mode'] = 0
         if op.padding == 'valid':
             attrs['auto_pad'] = 'VALID'
         elif op.padding == 'same':
@@ -33,7 +38,7 @@ def convert_keras_pooling_core(scope, operator, container, is_global, n_dims,
         else:
             raise RuntimeError("Unsupported padding type '{0}'".format(op.padding))
 
-    if channels_first:
+    if no_permutation_required:
         # In this case, the output of our Pool operator just match what Keras produces.
         container.add_node(op_type_prefix + onnx_op_type, adjusted_pooling_input,
                            operator.outputs[0].full_name, **attrs)
@@ -63,14 +68,14 @@ def convert_keras_max_pooling_2d(scope, operator, container):
 
 
 def convert_keras_max_pooling_3d(scope, operator, container):
-    input_perm_axes, output_perm_axes = get_permutation_config(2)
+    input_perm_axes, output_perm_axes = get_permutation_config(3)
     convert_keras_pooling_core(scope, operator, container, is_global=False, n_dims=3, op_type='Max',
                                input_perm_axes=input_perm_axes, output_perm_axes=output_perm_axes)
 
 
 def convert_keras_average_pooling_1d(scope, operator, container):
     input_perm_axes, output_perm_axes = get_permutation_config(1)
-    convert_keras_pooling_core(scope, operator, container, is_global=True, n_dims=1, op_type='Avg',
+    convert_keras_pooling_core(scope, operator, container, is_global=False, n_dims=1, op_type='Avg',
                                input_perm_axes=input_perm_axes, output_perm_axes=output_perm_axes)
 
 
