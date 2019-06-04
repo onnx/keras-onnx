8893c53e1c194057ff02775bb52fa3d3f0cb2cfa
Vinitra Swamy <vinitras@gmail.com>
2019-06-04 14:56:21 -0700

    updating alpha in thresholdedRelu

--- keras2onnx/common/onnx_ops.py
+++ keras2onnx/common/onnx_ops.py
@@ -9,10 +9,10 @@ from onnxconverter_common.onnx_ops import _create_name_or_use_existing_one
 
 def apply_thresholded_relu(scope, input_name, output_name, container, operator_name=None, alpha=None):
     if alpha == None:
-        alpha = [1.0]
-
+        alpha = 1.0
+    
     name = _create_name_or_use_existing_one(scope, 'ThresholdedRelu', operator_name)
-    attrs = {'name': name, 'alpha': alpha[0]}
+    attrs = {'name': name, 'alpha': alpha}
     if container.target_opset < 10:
     # ThresholdedRelu graduated from an experimental op to a full op in opset 10
     # onnxruntime maintains support in the ONNX domain for ThresholdedRelu as a contrib op
