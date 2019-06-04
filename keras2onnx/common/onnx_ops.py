94cec9ff8fb06a98f8dfcc640f949b011db06727
Vinitra Swamy <vinitras@gmail.com>
2019-06-04 12:19:37 -0700

    adding apply_thresholded_relu to onnx-ops to test thresRelu

--- keras2onnx/common/onnx_ops.py
+++ keras2onnx/common/onnx_ops.py
@@ -5,3 +5,18 @@
 ###############################################################################
 
 from onnxconverter_common.onnx_ops import *
+
+def apply_thresholded_relu(scope, input_name, output_name, container, operator_name=None, alpha=None):
+    if alpha == None:
+        alpha = [1.0]
+
+    name = _create_name_or_use_existing_one(scope, 'ThresholdedRelu', operator_name)
+    attrs = {'name': name, 'alpha': alpha[0]}
+    if container.target_opset < 10:
+    # ThresholdedRelu graduated from an experimental op to a full op in opset 10
+    # onnxruntime maintains support in the ONNX domain for ThresholdedRelu as a contrib op
+        attrs['op_domain'] = "ai.onnx"
+        op_version = 1
+    else:
+        op_version = 10
+    container.add_node('ThresholdedRelu', input_name, output_name, op_version=op_version, **attrs)
