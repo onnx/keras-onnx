6701d90f362e99f111e0114122c99c8564d31b36
Vinitra Swamy <vinitras@gmail.com>
2019-06-10 14:18:21 -0700

    remove apply_thres_relu since included in onnxconverter-common update

--- keras2onnx/common/onnx_ops.py
+++ keras2onnx/common/onnx_ops.py
@@ -5,19 +5,3 @@
 ###############################################################################
 
 from onnxconverter_common.onnx_ops import *
-from onnxconverter_common.onnx_ops import _create_name_or_use_existing_one
-
-def apply_thresholded_relu(scope, input_name, output_name, container, operator_name=None, alpha=None):
-    if alpha == None:
-        alpha = 1.0
-    
-    name = _create_name_or_use_existing_one(scope, 'ThresholdedRelu', operator_name)
-    attrs = {'name': name, 'alpha': alpha}
-    if container.target_opset < 10:
-    # ThresholdedRelu graduated from an experimental op to a full op in opset 10
-    # onnxruntime maintains support in the ONNX domain for ThresholdedRelu as a contrib op
-        attrs['op_domain'] = "ai.onnx"
-        op_version = 1
-    else:
-        op_version = 10
-    container.add_node('ThresholdedRelu', input_name, output_name, op_version=op_version, **attrs)
