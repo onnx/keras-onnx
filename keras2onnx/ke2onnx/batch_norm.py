Change default batch size to 'None' from 'N' Change size handling to use 'None' instead of None Fix BatchNorm parameter conversion to correctly carry over mean and variance

keras2onnx/ke2onnx/batch_norm.py
@@ -47,23 +47,20 @@ def convert_keras_batch_normalization(scope, operator, container):
     if not op.center:
         params.insert(1, np.zeros(params[1].shape, dtype=float))
 
-    gamma = params[0] / np.sqrt(params[3] + op.epsilon)
-    beta = params[1] - params[0] * params[2] / np.sqrt(params[3] + op.epsilon)
-
     scale_tensor_name = scope.get_unique_variable_name('scale')
-    container.add_initializer(scale_tensor_name, onnx_proto.TensorProto.FLOAT, params[0].shape, gamma)
+    container.add_initializer(scale_tensor_name, onnx_proto.TensorProto.FLOAT, params[0].shape, params[0])
     input_tensor_names.append(scale_tensor_name)
 
     bias_tensor_name = scope.get_unique_variable_name('bias')
-    container.add_initializer(bias_tensor_name, onnx_proto.TensorProto.FLOAT, params[1].shape, beta)
+    container.add_initializer(bias_tensor_name, onnx_proto.TensorProto.FLOAT, params[1].shape, params[1])
     input_tensor_names.append(bias_tensor_name)
 
     mean_tensor_name = scope.get_unique_variable_name('mean')
-    container.add_initializer(mean_tensor_name, onnx_proto.TensorProto.FLOAT, params[2].shape, 0 * params[2])
+    container.add_initializer(mean_tensor_name, onnx_proto.TensorProto.FLOAT, params[2].shape, params[2])
     input_tensor_names.append(mean_tensor_name)
 
     var_tensor_name = scope.get_unique_variable_name('var')
-    container.add_initializer(var_tensor_name, onnx_proto.TensorProto.FLOAT, params[3].shape, 1 + 0 * params[3])
+    container.add_initializer(var_tensor_name, onnx_proto.TensorProto.FLOAT, params[3].shape, params[3])
     input_tensor_names.append(var_tensor_name)
 
     epsilon = op.epsilon * 1e-3  # We use a much smaller epsilon because the original epsilon is absorbed in gamma