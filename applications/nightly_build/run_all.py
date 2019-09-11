b562c7d9d14349e944f5dab78c953860f999a348
David Fan <30608893+jiafatom@users.noreply.github.com>
2019-09-10 18:03:31 -0700

    Add mask rcnn to nightly build (#235)
    * mask rcnn
    
    * mask rcnn

--- applications/nightly_build/run_all.py
+++ applications/nightly_build/run_all.py
@@ -1,6 +1,12 @@
 import os
 from os import listdir
 from os.path import isfile, join
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--exclude')
+args = parser.parse_args()
+exclude_set = set(args.exclude.split()) if args.exclude is not None else set()
 
 mypath = '.'
 files = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f.find("test_") == 0]
@@ -8,9 +14,10 @@ files.sort()
 
 res_final = True
 for f_ in files:
-    res = os.system("pytest " + f_ +  " --doctest-modules --junitxml=junit/test-results-" + f_[5:-3] + ".xml")
-    if res > 0:
-        res_final = False
+    if f_ not in exclude_set:
+        res = os.system("pytest " + f_ +  " --doctest-modules --junitxml=junit/test-results-" + f_[5:-3] + ".xml")
+        if res > 0:
+            res_final = False
 
 if res_final:
     assert(True)
