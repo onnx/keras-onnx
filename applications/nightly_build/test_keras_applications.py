e7fdc54827777466cb7cd32dd023101528cd716d
David Fan <jiafa@microsoft.com>
2019-09-27 14:27:17 -0700

    Add a unit test: ZeroPadding2D (0, 0, 1, 1) + Conv2D to test MergePadConvSolution

--- applications/nightly_build/test_keras_applications.py
+++ applications/nightly_build/test_keras_applications.py
@@ -10,7 +10,7 @@ import unittest
 import keras2onnx
 import keras_segmentation
 import numpy as np
-from keras2onnx.proto import keras, is_keras_older_than
+from keras2onnx.proto import keras, is_tf_keras, is_keras_older_than
 from distutils.version import StrictVersion
 from os.path import dirname, abspath
 
@@ -18,6 +18,7 @@ sys.path.insert(0, os.path.join(dirname(abspath(__file__)), '../../tests/'))
 from test_utils import run_image, run_onnx_runtime
 img_path = os.path.join(os.path.dirname(__file__), '../data', 'street.jpg')
 
+K = keras.backend
 Activation = keras.layers.Activation
 AveragePooling2D = keras.layers.AveragePooling2D
 BatchNormalization = keras.layers.BatchNormalization
@@ -32,10 +33,13 @@ Input = keras.layers.Input
 LeakyReLU = keras.layers.LeakyReLU
 LSTM = keras.layers.LSTM
 MaxPooling2D = keras.layers.MaxPooling2D
+Model = keras.models.Model
 multiply = keras.layers.multiply
 Reshape = keras.layers.Reshape
 UpSampling2D = keras.layers.UpSampling2D
 ZeroPadding2D = keras.layers.ZeroPadding2D
+if not (is_keras_older_than("2.2.4") or is_tf_keras):
+    ReLU = keras.layers.ReLU
 
 Sequential = keras.models.Sequential
 
@@ -122,6 +126,62 @@ class TestKerasApplications(unittest.TestCase):
         data = np.random.rand(N, H, W, C).astype(np.float32).reshape((N, H, W, C))
         expected = model.predict(data)
         self.assertTrue(run_onnx_runtime(onnx_model.graph.name, onnx_model, data, expected, self.model_files))
+
+    # TODO: Move this to test_layers.py after this PR (https://github.com/microsoft/onnxconverter-common/pull/22) is in pypi.
+    @unittest.skipIf(is_keras_older_than("2.2.4"),
+                     "ReLU support requires keras 2.2.4 or later.")
+    def test_shared_model_3(self):
+        def _bottleneck(x, filters, activation, strides, block_id):
+            padding = 'same' if strides == 1 else 'valid'
+            ch_axis = 1 if K.image_data_format() == 'channels_first' else -1
+            if strides > 1:
+                x = ZeroPadding2D(((0, 1), (0, 1)), data_format=K.image_data_format())(x)
+
+            x = Conv2D(filters // 2, (1, 1), padding='same', name=f'bottleneck_{block_id}_conv_0',
+                          use_bias=False, data_format=K.image_data_format())(x)
+
+            x = BatchNormalization(axis=ch_axis, name=f'bottleneck_{block_id}_bnorm_0')(x)
+
+            if activation == 'relu':
+                x = ReLU(name=f'bottleneck_{block_id}_relu_0')(x)
+            elif activation == 'leaky':
+                x = LeakyReLU(name=f'bottleneck_{block_id}_leaky_0')(x)
+            else:
+                assert False
+
+            x = Conv2D(filters // 2, (3, 3), padding=padding, name=f'bottleneck_{block_id}_conv_1',
+                          strides=strides, use_bias=False, data_format=K.image_data_format())(x)
+            x = BatchNormalization(axis=ch_axis, name=f'bottleneck_{block_id}_bnorm_1')(x)
+            if activation == 'relu':
+                x = ReLU(name=f'bottleneck_{block_id}_relu_1')(x)
+            elif activation == 'leaky':
+                x = LeakyReLU(name=f'bottleneck_{block_id}_leaky_1')(x)
+            else:
+                assert False
+
+            x = Conv2D(filters, (1, 1), padding='same', name=f'bottleneck_{block_id}_conv_2',
+                          use_bias=False, data_format=K.image_data_format())(x)
+            x = BatchNormalization(axis=ch_axis, name=f'bottleneck_{block_id}_bnorm_2')(x)
+            if activation == 'relu':
+                x = ReLU(name=f'bottleneck_{block_id}_relu_2')(x)
+            elif activation == 'leaky':
+                x = LeakyReLU(name=f'bottleneck_{block_id}_leaky_2')(x)
+            else:
+                assert False
+
+            return x
+
+        def convnet_7(input_shape, activation):
+            input = Input(shape=input_shape, name='input_1')
+            x = _bottleneck(input, filters=16, strides=1, activation=activation, block_id=1)
+            x = _bottleneck(x, filters=32, strides=2, activation=activation, block_id=2)
+            return Model(inputs=input, outputs=x, name='convnet_7')
+
+        model = convnet_7(input_shape=(3, 96, 128), activation='relu')
+        onnx_model = keras2onnx.convert_keras(model, model.name)
+        x = np.random.rand(1, 3, 96, 128).astype(np.float32)
+        expected = model.predict(x)
+        self.assertTrue(run_onnx_runtime(onnx_model.graph.name, onnx_model, x, expected, self.model_files))
 
 
 if __name__ == "__main__":
