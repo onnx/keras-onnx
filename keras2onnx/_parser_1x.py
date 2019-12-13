0167817dfe08bc6333ff435112fc10bb7a3f8d4a
David Fan <jiafa@microsoft.com>
2019-12-13 14:19:52 -0800

    Use inbound in/output_shape as onnx tensor in/output_shape

--- keras2onnx/_parser_1x.py
+++ keras2onnx/_parser_1x.py
@@ -23,10 +23,10 @@ def on_parsing_keras_layer(graph, node_list, layer, kenode, model, varset, prefi
     if prefix is None:  # prefix is designed for the distinguish among the shared model instances.
         prefix = ''
 
-    for i_ in inputs:
+    for n_, i_ in enumerate(inputs):
         iname = prefix + i_.name
         k2o_logger().debug('input : ' + iname)
-        var_type = adjust_input_batch_size(infer_variable_type(i_, varset.target_opset))
+        var_type = adjust_input_batch_size(infer_variable_type(i_, varset.target_opset, kenode.input_shapes[n_]))
         i0 = varset.get_local_variable_or_declare_one(iname, var_type)
         operator.add_input(i0)
 
@@ -41,7 +41,7 @@ def on_parsing_keras_layer(graph, node_list, layer, kenode, model, varset, prefi
     for n_, o_ in enumerate(outputs):
         oname = prefix + o_.name
         k2o_logger().debug('output: ' + oname)
-        o1 = varset.get_local_variable_or_declare_one(oname, infer_variable_type(o_, varset.target_opset))
+        o1 = varset.get_local_variable_or_declare_one(oname, infer_variable_type(o_, varset.target_opset, kenode.output_shapes[n_]))
         operator.add_output(o1)
 
     if hasattr(layer, 'output_mask') and layer.output_mask is not None:
