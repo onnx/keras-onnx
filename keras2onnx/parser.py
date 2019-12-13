Fixing the issue of Masking layer with a custom layer.

keras2onnx/parser.py
@@ -16,7 +16,8 @@ from .wrapper import tf2onnx_wrap, TFNODES
 from ._builtin import TYPES
 from ._parse_tf import (infer_variable_type, LayerInfo, is_placeholder_node,
                         tsname_to_node, on_parsing_keras_layer_v2, adjust_input_batch_size as _adjust_input_batch_size)
-from ._parser_1x import (extract_inbound_nodes, list_input_tensors,
+from ._parser_1x import (extract_inbound_nodes,
+                         list_input_tensors, list_input_mask, list_output_mask,
                          list_output_tensors, list_input_shapes, list_output_shapes, on_parsing_keras_layer)
 
 
@@ -287,7 +288,7 @@ def _on_parsing_tf_nodes(nodelist, varset):
     for node_ in nodelist:
         cvt = get_converter(node_.type)
         assert cvt is not None, "Cannot find the tf.op({}) converter.".format(node_.type)
-
+        k2o_logger().debug("Processing a tf node - %s" % node_.name)
         operator = varset.declare_local_operator(node_.type, raw_model=node_, op_name=node_.name)
 
         for o_ in node_.outputs:
@@ -445,7 +446,7 @@ def _build_inference_nodeset(graph, outputs):
 
 
 def _create_keras_nodelist(layer, inference_nodeset, out_node=None):
-    newly = set()
+    newly = list()
     ts_end = set()  # the input tensor set of the whole layer/model.
     for node_ in extract_inbound_nodes(layer):
         if out_node is not None and out_node.name not in \
@@ -453,12 +454,20 @@ def _create_keras_nodelist(layer, inference_nodeset, out_node=None):
             continue  # this layer could be reused several times in the whole graph.
         if any(ts_.op not in inference_nodeset for ts_ in list_output_tensors(node_)):
             continue
-        newly |= set([ts_.op for ts_ in list_output_tensors(node_)])
+        newly.extend([ts_.op for ts_ in list_output_tensors(node_)])
         ts_end |= set(list_input_tensors(node_))
 
+    for ts_ in list_input_mask(layer):
+        ts_end.add(ts_)
+
+    for ts_ in list_output_mask(layer):
+        newly.append(ts_.op)
+
     visited = set()
+    nodelist = list()  # keep the node list order.
     while newly:
-        visited |= newly
+        visited.update(newly)
+        nodelist.extend(newly)
         newly.clear()
         for n_ in visited:
             for i_ in n_.inputs:
@@ -472,9 +481,9 @@ def _create_keras_nodelist(layer, inference_nodeset, out_node=None):
                     else:
                         continue
 
-                newly.add(i_.op)
+                newly.append(i_.op)
 
-    return list(visited)
+    return nodelist
 
 
 def _general_nodelist_closure(node, nodeset, keras_nodeset):
@@ -513,19 +522,13 @@ def _build_keras_nodeset(inference_nodeset, keras_node_dict):
     return nodes
 
 
-def _get_output_nodes(node_list, layer, node):
-    if layer:
-        for nd_ in extract_inbound_nodes(layer):
-            name_set = set(tsname_to_node(ts_.name) for ts_ in list_output_tensors(nd_))
-            if node.name in name_set:
-                return set(n_ for n_ in node_list if n_.name in name_set)
-    else:
-        nodes_has_children = set()
-        for node in node_list:
-            if node:
-                for input_tensor in node.inputs:
-                    nodes_has_children.add(input_tensor.op)
-        return set(node_list) - nodes_has_children
+def _get_output_nodes(node_list, node):
+    nodes_has_children = set()
+    for node in node_list:
+        if node:
+            for input_tensor in node.inputs:
+                nodes_has_children.add(input_tensor.op)
+    return [n_ for n_ in node_list if n_ not in nodes_has_children]  # need to keep the order.
 
 
 def _advance_by_input(cur_node, layer_nodes, subgraph, inputs, graph_inputs, q_overall):
@@ -545,8 +548,7 @@ def _visit_nodelist(activated_keras_nodes, input_nodes, layer_key,
                     keras_node_dict, node, nodelist, q_overall, visited):
     subgraph = list()
     i_subgraph = set()
-    for ot_ in (_get_output_nodes(activated_keras_nodes, layer_key, node
-                                  ) if activated_keras_nodes else [node]):
+    for ot_ in (_get_output_nodes(activated_keras_nodes, node) if activated_keras_nodes else [node]):
         if ot_ not in nodelist:
             visited.add(ot_)
             nodelist.append(ot_)
@@ -629,7 +631,7 @@ def _parse_graph_core(graph, keras_node_dict, topology, top_scope, output_names)
         nodes = []
         layer_key_, model_ = _parse_nodes(graph, inference_nodeset, input_nodes, keras_node_dict, keras_nodeset,
                                           node, nodes, varset, visited, q_overall)
-        if not nodes:  # already processed by the parse_nodes_XX
+        if not nodes:  # already processed by the _parse_nodes
             continue
 
         k2o_logger().debug('Processing a keras layer - (%s: %s)' % (layer_key_.name, type(layer_key_)) if
@@ -640,6 +642,8 @@ def _parse_graph_core(graph, keras_node_dict, topology, top_scope, output_names)
             _on_parsing_tf_subgraph(graph, nodes, varset)
         else:
             kenode = _find_kenode_by_output_tensor(extract_inbound_nodes(layer_key_), nodes[0].name)
+            if kenode is None:
+                kenode = extract_inbound_nodes(layer_key_)[0]
             on_parsing_keras_layer(graph, nodes, layer_key_, kenode, model_, varset)
 
     for nd_ in input_nodes:

tests/test_layers.py
@@ -68,7 +68,6 @@ ZeroPadding2D = keras.layers.ZeroPadding2D
 if not (is_keras_older_than("2.2.4") or is_tf_keras):
     ReLU = keras.layers.ReLU
 
-
 debug_mode = False
 if os.environ.get('debug_mode', '0') != '0':
     debug_mode = True
@@ -120,7 +119,8 @@ class TestKerasTF2ONNX(unittest.TestCase):
         self.assertTrue(run_onnx_runtime('onnx_bias_add', onnx_model, data, expected, self.model_files))
 
         model = Sequential()
-        model.add(Lambda(lambda x: tf.nn.bias_add(x, tf.constant([100., -100.]), data_format='NCHW'), input_shape=[2, 3, 4]))
+        model.add(
+            Lambda(lambda x: tf.nn.bias_add(x, tf.constant([100., -100.]), data_format='NCHW'), input_shape=[2, 3, 4]))
         onnx_model = keras2onnx.convert_keras(model, 'test_tf_bias_add')
         data = np.random.rand(5, 2, 3, 4).astype(np.float32)
         expected = model.predict(data)
@@ -204,6 +204,19 @@ class TestKerasTF2ONNX(unittest.TestCase):
         data_2 = np.array([3]).astype(np.float32).reshape(1, 1)
         expected = model.predict([data_1, data_2])
         self.assertTrue(run_onnx_runtime('onnx_range_2', onnx_model, [data_1, data_2], expected, self.model_files))
+
+    def test_tf_not_equal(self):
+        input1_shape = [[3], [3]]
+        input1 = Input(shape=input1_shape[0], dtype='int32')
+        input2 = Input(shape=input1_shape[1], dtype='int32')
+        comp = Lambda(lambda x: tf.not_equal(x[0], x[1]))([input1, input2])
+        model = keras.models.Model(inputs=[input1, input2], outputs=comp)
+
+        onnx_model = keras2onnx.convert_keras(model, 'tf_not_equal')
+        data1 = np.array([[1, 2, 3], [1, 2, 3]])
+        data2 = np.array([[1, 2, 3], [2, 1, 4]])
+        expected = model.predict([data1, data2])
+        self.assertTrue(run_onnx_runtime('tf_not_equal', onnx_model, [data1, data2], expected, self.model_files))
 
     def test_tf_realdiv(self):
         input1_shape = [(2, 3), (2, 3)]
@@ -1441,6 +1454,40 @@ class TestKerasTF2ONNX(unittest.TestCase):
         model = Sequential([
             keras.layers.Masking(mask_value=0., input_shape=(timesteps, features)),
             LSTM(8, return_state=False, return_sequences=False)
+        ])
+
+        onnx_model = keras2onnx.convert_keras(model, model.name)
+        x = np.random.uniform(100, 999, size=(2, 3, 5)).astype(np.float32)
+        expected = model.predict(x)
+        self.assertTrue(run_onnx_runtime(onnx_model.graph.name, onnx_model, x, expected, self.model_files))
+
+    def test_masking_custom(self):
+        class MyPoolingMask(keras.layers.Layer):
+            def __init__(self, **kwargs):
+                self.supports_masking = True
+                super(MyPoolingMask, self).__init__(**kwargs)
+
+            def build(self, input_shape):
+                super(MyPoolingMask, self).build(input_shape)
+
+            def compute_mask(self, inputs, input_mask=None):
+                return None
+
+            def call(self, inputs, mask=None, **kwargs):
+                if mask is not None:
+                    return K.sum(inputs, axis=-2) / (
+                            K.sum(K.cast(mask, K.dtype(inputs)), axis=-1, keepdims=True) + K.epsilon())
+                else:
+                    output = K.mean(inputs, axis=-2)
+                    return output
+
+            def compute_output_shape(self, input_shape):
+                return input_shape[:-2] + input_shape[-1:]
+
+        timesteps, features = (3, 5)
+        model = Sequential([
+            keras.layers.Masking(mask_value=0., input_shape=(timesteps, features)),
+            MyPoolingMask()
         ])
 
         onnx_model = keras2onnx.convert_keras(model, model.name)