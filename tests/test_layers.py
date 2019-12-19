c78d7ded536cc45e8ba00376e736e16a6eb4b476
David Fan <jiafa@microsoft.com>
2019-12-19 14:53:22 -0800

    Convert tf.maximum and tf.minimum using keras _builtin

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -163,6 +163,52 @@ class TestKerasTF2ONNX(unittest.TestCase):
         expected = model.predict(data)
         self.assertTrue(run_onnx_runtime('onnx_tf_gather', onnx_model, data, expected, self.model_files))
 
+    def test_tf_maximum_minimum(self):
+        input1_shape_list = [(2, 3), (2, 3)]
+        input2_shape_list = [(2, 3), (2, 1)]
+
+        def my_func_1(x):
+            return tf.minimum(tf.maximum(x[0], x[1]), 0.5)
+
+        def my_func_2(x):
+            return tf.minimum(tf.maximum(x[0], 0.5), x[1])
+
+        for idx_ in range(len(input1_shape_list)):
+            for myFunc in [my_func_1, my_func_2]:
+                input1 = Input(shape=input1_shape_list[idx_], dtype=tf.float32)
+                input2 = Input(shape=input2_shape_list[idx_], dtype=tf.float32)
+                added = Lambda(myFunc)([input1, input2])
+                model = keras.models.Model(inputs=[input1, input2], outputs=added)
+
+                onnx_model = keras2onnx.convert_keras(model, 'tf_maximum_minimum')
+                batch_data1_shape = (2,) + input1_shape_list[idx_]
+                batch_data2_shape = (2,) + input2_shape_list[idx_]
+                data1 = np.random.rand(*batch_data1_shape).astype(np.float32)
+                data2 = np.random.rand(*batch_data2_shape).astype(np.float32)
+                expected = model.predict([data1, data2])
+                self.assertTrue(run_onnx_runtime('tf_maximum_minimum', onnx_model, [data1, data2], expected, self.model_files))
+
+        def my_func_3(x):
+            return tf.minimum(tf.maximum(x[0], x[1]), 50)
+
+        def my_func_4(x):
+            return tf.minimum(tf.maximum(x[0], 50), x[1])
+
+        for idx_ in range(len(input1_shape_list)):
+            for myFunc in [my_func_3, my_func_4]:
+                input1 = Input(shape=input1_shape_list[idx_], dtype=tf.int32)
+                input2 = Input(shape=input2_shape_list[idx_], dtype=tf.int32)
+                added = Lambda(myFunc)([input1, input2])
+                model = keras.models.Model(inputs=[input1, input2], outputs=added)
+
+                onnx_model = keras2onnx.convert_keras(model, 'tf_maximum_minimum')
+                batch_data1_shape = (2,) + input1_shape_list[idx_]
+                batch_data2_shape = (2,) + input2_shape_list[idx_]
+                data1 = (100 * np.random.rand(*batch_data1_shape)).astype(np.int32)
+                data2 = (100 * np.random.rand(*batch_data2_shape)).astype(np.int32)
+                expected = model.predict([data1, data2])
+                self.assertTrue(run_onnx_runtime('tf_maximum_minimum', onnx_model, [data1, data2], expected, self.model_files))
+
     def test_tf_pad(self):
         def my_func_1(x):
             paddings = tf.constant([[0, 0], [1, 3], [2, 4]])
