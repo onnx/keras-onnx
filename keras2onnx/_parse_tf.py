c71cadcb9fe629bd8ffd814039e4974ae2d3def2
Wenbing Li <wenbingl@outlook.com>
2020-03-23 15:54:51 -0700

    support tensorflow 2.2 and some fixing related to subclassed.

--- keras2onnx/_parse_tf.py
+++ keras2onnx/_parse_tf.py
@@ -10,7 +10,7 @@ from .common import k2o_logger, get_default_batch_size
 from .funcbook import get_converter
 
 from .proto import keras
-from .proto.tfcompat import normalize_tensor_shape
+from .proto.tfcompat import normalize_tensor_shape, is_subclassed
 from .ke2onnx import keras_layer_spec
 from ._builtin import TYPES, is_placeholder_node, tsname_to_node
 
@@ -115,8 +115,21 @@ class LayerInfo(object):
 
 
 def is_subclassing(model):
-    return not (model._is_graph_network or  # pylint:disable=protected-access
-                isinstance(model, keras.engine.sequential.Sequential))
+    subclassed = not (model._is_graph_network or  # pylint:disable=protected-access
+                      isinstance(model, keras.engine.sequential.Sequential))
+    if subclassed:
+        return True
+
+    def subclassed_layer(layer):
+        if hasattr(layer, 'layers'):
+            if any(is_subclassed(l_) for l_ in layer.layers):
+                return True
+            for l_ in layer.layers:
+                if subclassed_layer(l_):
+                    return True
+        return False
+
+    return subclassed_layer(model)
 
 
 def _get_layers(tf_utils, layer):
@@ -204,7 +217,7 @@ def build_layer_outputs(model, graph, outputs):
     return output_dict
 
 
-def extract_outputs_from_subclassing_model(model, output_dict, output_names):
+def extract_outputs_from_subclassing_model(model, output_dict, input_names, output_names):
     from tensorflow.python.keras.saving import saving_utils as _saving_utils
     from tensorflow.python.util import object_identity
     from ._graph_cvt import convert_variables_to_constants_v2 as _convert_to_constants
@@ -215,6 +228,11 @@ def extract_outputs_from_subclassing_model(model, output_dict, output_names):
     output_dict.update(build_layer_outputs(model, concrete_func.graph, concrete_func.outputs))
     graph_def, converted_input_indices = _convert_to_constants(
         concrete_func, lower_control_flow=True)
+    input_tensors = concrete_func.graph.internal_captures
+    converted_inputs = object_identity.ObjectIdentitySet(
+        [input_tensors[index] for index in converted_input_indices])
+    input_names.extend([
+        tensor.name for tensor in concrete_func.inputs if tensor not in converted_inputs])
 
     with tf.Graph().as_default() as tf_graph:
         tf.import_graph_def(graph_def, name='')
@@ -240,10 +258,10 @@ def extract_outputs_from_inbound_nodes(model):
     return output_dict
 
 
-def build_layer_output_from_model(model, output_dict, output_names):
+def build_layer_output_from_model(model, output_dict, input_names, output_names):
     if is_subclassing(model):
         tf.compat.v1.enable_tensor_equality()  # re-enable tensor tensor equality for subclassing model.
-        return extract_outputs_from_subclassing_model(model, output_dict, output_names)
+        return extract_outputs_from_subclassing_model(model, output_dict, input_names, output_names)
     else:
         graph = model.outputs[0].graph
         output_names.extend([n.name for n in model.outputs])
