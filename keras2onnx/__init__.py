4d709523de74b8507c858180673456346706072e
Wenbing Li <wenbingl@outlook.com>
2020-04-02 19:15:45 -0700

    update the version and requirements

--- keras2onnx/__init__.py
+++ keras2onnx/__init__.py
@@ -7,7 +7,7 @@
 keras2onnx
 This package converts keras models into ONNX for use with any inference engine supporting ONNX
 """
-__version__ = "1.6.0"
+__version__ = "1.6.1"
 __author__ = "Microsoft Corporation"
 __producer__ = "keras2onnx"
 
