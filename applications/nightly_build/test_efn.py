ace2776be7dc3a259628267a291e5105b3d87b30
David Fan <jiafa@microsoft.com>
2020-04-10 15:29:20 -0700

    Add efficientnet.tfkeras to nightly build

--- applications/nightly_build/test_efn.py
+++ applications/nightly_build/test_efn.py
@@ -6,17 +6,16 @@
 import os
 import sys
 import unittest
+import keras2onnx
 from os.path import dirname, abspath
 from keras2onnx.proto import keras, is_keras_older_than
 
 sys.path.insert(0, os.path.join(dirname(abspath(__file__)), '../../tests/'))
-from test_utils import run_image
+from test_utils import run_image, run_onnx_runtime
 
 img_path = os.path.join(os.path.dirname(__file__), '../data', 'street.jpg')
 
 
-# @unittest.skipIf(is_keras_older_than('2.2.0'), "efficientnet needs keras >= 2.2.0")
-@unittest.skip("Minor discrepancy on the model output.")
 class TestEfn(unittest.TestCase):
 
     def setUp(self):
@@ -26,6 +25,7 @@ class TestEfn(unittest.TestCase):
         for fl in self.model_files:
             os.remove(fl)
 
+    @unittest.skip("Minor discrepancy on the model output.")
     def test_custom(self):
         from efficientnet import keras as efn
         keras.backend.set_learning_phase(0)
@@ -34,12 +34,22 @@ class TestEfn(unittest.TestCase):
         res = run_image(backbone, self.model_files, img_path, target_size=(600, 600), rtol=1e-1)
         self.assertTrue(*res)
 
+    @unittest.skip("Minor discrepancy on the model output.")
     def test_efn(self):
         from efficientnet import keras as efn
         keras.backend.set_learning_phase(0)
         model = efn.EfficientNetB7(weights='imagenet')
         res = run_image(model, self.model_files, img_path, target_size=(600, 600), rtol=1e-1)
         self.assertTrue(*res)
+
+    def test_efn_2(self):
+        import efficientnet.tfkeras as efn
+        import numpy as np
+        data = np.random.rand(1, 224, 224, 3).astype(np.float32)
+        model = efn.EfficientNetB0(weights='imagenet')
+        expected = model.predict(data)
+        onnx_model = keras2onnx.convert_keras(model, model.name)
+        self.assertTrue(run_onnx_runtime('onnx_efn_2', onnx_model, data, expected, self.model_files))
 
 
 if __name__ == "__main__":
