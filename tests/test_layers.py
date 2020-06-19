Fix tf.keras.layers.Softmax conversion

tests/test_layers.py
@@ -1373,6 +1373,17 @@ def test_Softmax(advanced_activation_runner):
     advanced_activation_runner(layer, data)
 
 
+def test_Softmax_2(runner):
+    keras.backend.set_image_data_format("channels_first")
+    model = keras.Sequential()
+    model.add(keras.layers.InputLayer((2, 4, 4)))
+    model.add(keras.layers.Softmax(axis=1))
+    data = np.random.rand(1, 2, 4, 4).astype(np.float32)
+    onnx_model = keras2onnx.convert_keras(model, model.name)
+    expected = model.predict(data)
+    assert runner(onnx_model.graph.name, onnx_model, data, expected)
+
+
 @pytest.mark.skipif(is_tensorflow_older_than('1.14.0') and is_tf_keras, reason='old tf version')
 def test_tf_nn_activation(runner):
     for activation in ['relu', tf.nn.relu, tf.nn.relu6, tf.nn.softmax, tf.nn.leaky_relu]:
@@ -2399,7 +2410,8 @@ def test_sub_model(runner):
 
     model = Sequential()  # 28, 28, 1
     model.add(Conv2D(32, kernel_size=(3, 3), activation='relu',
-                     input_shape=input_shape, padding='valid'))  # 28, 28, 1
+                     input_shape=input_shape, padding='valid',
+                     data_format='channels_last'))  # 28, 28, 1
     model.add(Conv2D(64, (3, 3), activation='relu', padding='valid'))  # 28, 28, 1
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding="valid"))  # 14, 14, 1
     model.add(Dropout(0.25))