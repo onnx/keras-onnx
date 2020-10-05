83b4d2d17cae305a3fce200af5ccc00346c4ad06
David Fan <30608893+jiafatom@users.noreply.github.com>
2020-10-04 22:31:23 -0700

    Add ResNext model conversion to nightly build (#628)
    * test
    
    * test

---
+++ applications/nightly_build/run_all_v2.py
@@ -0,0 +1,15 @@
+import os
+
+files = ['test_keras_applications_v2.py', 'test_transformers.py', 'test_efn.py', 'test_resnext.py']
+files.sort()
+
+res_final = True
+for f_ in files:
+    res = os.system("pytest " + f_ +  " --doctest-modules --junitxml=junit/test-results-" + f_[5:-3] + ".xml")
+    if res > 0:
+        res_final = False
+
+if res_final:
+    assert(True)
+else:
+    assert(False)
